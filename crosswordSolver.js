08bfde1decd4ddc3797883f949fe6f200fdcbde2
eabderrahma <elmahmoudiabderrahmane@gmail.com>
2025-06-23 21:50:48 +0100

    working on phase 2, loop into the grid rows and cols

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -15,12 +15,52 @@ function parsPuzzle(puzzleString) {
         }
         grid.push(cells); //add row in the grid
     }
+    //print 2d array
     return grid;
 }
 
+//phase 2 creat a list of valid works
+function findSlots(grid) {
+    const slots = [];
+    // let res = parsPuzzle(grid);
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    // console.log("rows = ", rows)
+    // console.log("cols = ", cols)
+    // console.log(grid)
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            const cell = grid[r][c];
+
+            if (cell != 1 && cell != 2) {
+                continue;
+            }
+        }
+    }
+
+}
+
+
+
+const puzzle = `...1...........
+..1000001000...
+...0....0......
+.1......0...1..
+.0....100000000
+100000..0...0..
+.0.....1001000.
+.0.1....0.0....
+.10000000.0....
+.0.0......0....
+.0.0.....100...
+...0......0....
+..........0....`
 
 const puzzleString = "2001\n0..0\n1000\n0..0";
-console.log(parsPuzzle(puzzleString));
+console.log(findSlots(puzzle));
+// console.log((parsPuzzle(findSlots(puzzleString))))
 
 
 // function crosswordSolver(puzzleString, wordList) {
