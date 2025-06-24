49ec60ad66d32e9bdda8573191b568d41269ddb9
oerraoui <itsscarl2022@gmail.com>
2025-06-24 12:55:15 +0100

    refactor: improve input validation and enhance findSlots function

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 function parse(puzzleString){
-    if(typeof puzzleString != 'string') {
+    if(typeof puzzleString !== 'string' || puzzleString === ""){
         return null
     }
     return puzzleString.split('\n')
 }
 
+function findSlots(arr){
+   console.log(arr)
 
+let w = 0 
+let h = 0 
+    while(w < arr.length){
+        console.log("W : " ,arr[w])
+        h = 0
+        while(h < arr[w].length) {
+            console.log("h:",arr[w][h])
+            h++
+        }
+        w++
+    }
+
+}
 
 
 function main(puzle , w) {
+    // console.log("First input =>", puzle)
     if(!puzle || !w || w.length == 0){
         console.log('Error');
         return
     }
-    console.log(w.length)
-    console.log(new Set(w).size )
+    // console.log(w.length)
+    // console.log(new Set(w).size )
     if(new Set(w).size !== w.length) {
         console.log('Error')
         return
     }
+    
+    const grid = parse(puzle)
+    // console.log("Grided parse(puzzle) =>" , grid)
+    if(!grid) {
+        console.log('Error')
+        return
+    }
+
+    const slots = findSlots(grid)
+
 }
 
 
